b6f1a0206e2c3aa75c4111929f8136b2f9bb66ab
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2018-07-07 09:59:05 -0600

    React app initial commit, add router

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Order Book Front End
+      </div>
+    );
+  }
+}
+
+export default App;
