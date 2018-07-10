9da252565dc21936912c6c9988b3ace986b37716
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2018-07-09 21:22:24 -0600

    App test and snap

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import renderer from 'react-test-renderer';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({ adapter: new Adapter() });
+
+function setup() {
+  const comp = shallow(<App />);
+
+  return { comp };
+}
+
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    const { comp } = setup();
+    expect(comp).toBeDefined();
+  });
+
+  test('snapshot', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
